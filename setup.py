e46a48ad7ad8f0c253d16b785ef100359f30b8a8
Sébastien Weber <seba.weber@gmail.com>
2021-01-07 23:43:55 +0100

    added VERSION file when building the package (graft src in manifest)

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     version=version.get_version(),
     packages=find_packages(),
     package_data={'': ['*.dll']},
+    include_package_data=True,
     entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}'},
     install_requires=[
         'pymodaq>=2.0',
