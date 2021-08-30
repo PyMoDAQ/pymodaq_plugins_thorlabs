68296fd4a959059c6db701117a466b4a10001b30
BRESTEAU David <david.bresteau@cea.fr>
2021-08-30 18:15:29 +0200

    Fix. The name of the dependency was wrong.

--- plugin_info.py
+++ plugin_info.py
@@ -7,5 +7,5 @@ author = 'Sebastien Weber'
 author_email = 'sebastien.weber@cemes.cnrs.fr'
 
 #packages required for your plugin:
-packages_required = ['pythonnet', 'pywin32', 'instrumental', 'nicelib', 'pymodaq']
+packages_required = ['pythonnet', 'pywin32', 'instrumental-lib', 'nicelib', 'pymodaq']
 ##
