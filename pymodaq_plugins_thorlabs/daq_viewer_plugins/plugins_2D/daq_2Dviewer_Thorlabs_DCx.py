e4ff664824da78a0af93c4d5f134b211e598357c
Sébastien Weber <seba.weber@gmail.com>
2021-09-28 10:18:39 +0200

    added methods to change the exposure

--- pymodaq_plugins_thorlabs/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Thorlabs_DCx.py
+++ pymodaq_plugins_thorlabs/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Thorlabs_DCx.py
@@ -1,8 +1,8 @@
 import numpy as np
 from easydict import EasyDict as edict
 from pymodaq.daq_utils.daq_utils import ThreadCommand, getLineInfo, DataFromPlugins, Axis
-from pymodaq.daq_viewer.utility_classes import DAQ_Viewer_base, comon_parameters
-from instrumental import instrument, list_instruments
+from pymodaq.daq_viewer.utility_classes import DAQ_Viewer_base, comon_parameters, main
+from instrumental import instrument, list_instruments, Q_
 
 class DAQ_2DViewer_Thorlabs_DCx(DAQ_Viewer_base):
     """This plugin is intended for Thorlabs DCx cameras series.
@@ -35,10 +35,8 @@ class DAQ_2DViewer_Thorlabs_DCx(DAQ_Viewer_base):
         serial_numbers.append(camera.serial.decode("utf-8"))
 
     params = comon_parameters + [
-        {'title': 'Serial number:',
-         'name': 'serial_number',
-         'type': 'itemselect',
-         'value': dict(all_items=serial_numbers, selected=[serial_numbers[0]])},
+        {'title': 'Serial number:', 'name': 'serial_number', 'type': 'list', 'values': serial_numbers},
+        {'title': 'Exposure (ms):', 'name': 'exposure', 'type': 'float', 'value': 9.},
     ]
 
     def __init__(self, parent=None, params_state=None):
@@ -52,6 +50,9 @@ class DAQ_2DViewer_Thorlabs_DCx(DAQ_Viewer_base):
     def commit_settings(self, param):
         """
         """
+        if param.name() == 'exposure':
+            self.controller.exposure = Q_(param.value(), 'ms')
+            self.settings.child('exposure').setValue(self.controller.exposure.m_as('ms'))
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -77,26 +78,20 @@ class DAQ_2DViewer_Thorlabs_DCx(DAQ_Viewer_base):
                 else:
                     self.controller = controller
             else:
-                camera_serial = self.settings.child('serial_number').value()['selected']
+                camera_serial = self.settings.child('serial_number').value()
                 plugged_cameras = list_instruments(module='cameras.uc480')
                 selected_camera = None
                 # Find the paramset that has the selected serial number
                 for paramset in plugged_cameras:
                     camera = instrument(paramset, reopen_policy='reuse')
-                    if camera.serial.decode("utf-8") == camera_serial[0]:
+                    if camera.serial.decode("utf-8") == camera_serial:
                         selected_camera = camera
 
                 self.controller = selected_camera
+                self.settings.child('exposure').setValue(self.controller.exposure.m_as('ms'))
+                self.settings.child('exposure').setOpts(
+                    limits=[exp.m_as('ms') for exp in self.controller._get_exposure_range()])
 
-            image = self.controller.grab_image()
-
-            data_x_axis = image[0, :]
-            self.x_axis = Axis(data=data_x_axis, label='', units='')
-            self.emit_x_axis()
-
-            data_y_axis = image[:, 0]
-            self.y_axis = Axis(data=data_y_axis, label='', units='')
-            self.emit_y_axis()
 
             self.status.info = "Detector initialized"
             self.status.initialized = True
@@ -123,46 +118,17 @@ class DAQ_2DViewer_Thorlabs_DCx(DAQ_Viewer_base):
         Naverage: (int) Number of hardware averaging
         kwargs: (dict) of others optionals arguments
         """
-
-        data_tot = [np.array(self.controller.grab_image())]
-        self.data_grabed_signal.emit([DataFromPlugins(name='Mock1', data=data_tot,
-                                                      dim='Data2D', labels=['dat0'])])
+        data = self.controller.grab_image(exposure_time=Q_(self.settings.child('exposure').value(), 'ms'))
+        self.data_grabed_signal.emit([DataFromPlugins(name='Thorcam', data=[data],
+                                                      dim='Data2D')])
 
     def stop(self):
 
         self.controller.stop_live_video()
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        #self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
 
         return ''
 
 
-def main():
-    """
-    this method start a DAQ_Viewer object with this defined plugin as detector
-    Returns
-    -------
-    """
-    import sys
-    from PyQt5 import QtWidgets
-    from pymodaq.daq_utils.gui_utils import DockArea
-    from pymodaq.daq_viewer.daq_viewer_main import DAQ_Viewer
-    from pathlib import Path
-
-    app = QtWidgets.QApplication(sys.argv)
-    win = QtWidgets.QMainWindow()
-    area = DockArea()
-    win.setCentralWidget(area)
-    win.resize(1000, 500)
-    win.setWindowTitle('PyMoDAQ Viewer')
-    detector = Path(__file__).stem[13:]
-    det_type = f'DAQ{Path(__file__).stem[4:6].upper()}'
-    prog = DAQ_Viewer(area, title="Testing", DAQ_type=det_type)
-    win.show()
-    prog.detector = detector
-    prog.init_det()
-
-    sys.exit(app.exec_())
-
-
 if __name__ == '__main__':
-    main()
+    main(__file__)
