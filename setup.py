ae8f59e2271b98bff8397040ed37bd214133f136
Sébastien Weber <seba.weber@gmail.com>
2021-11-10 21:55:33 +0100

    src and MIT license

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
-from plugin_info import SHORT_PLUGIN_NAME, packages_required, package_url, author_email, author, description
-PLUGIN_NAME = f'pymodaq_plugins_{SHORT_PLUGIN_NAME}'
+from setuptools import setup, find_packages
+import toml
+
+config = toml.load('./plugin_info.toml')
+SHORT_PLUGIN_NAME = config['plugin-info']['SHORT_PLUGIN_NAME']
+PLUGIN_NAME = f"pymodaq_plugins_{config['plugin-info']['SHORT_PLUGIN_NAME']}"
 
 
-import importlib
-import sys
-try:
-    import setuptools
-    from setuptools import setup, find_packages
-    from setuptools.command import install
-except ImportError:
-    sys.stderr.write("Warning: could not import setuptools; falling back to distutils.\n")
-    from distutils.core import setup
-    from distutils.command import install
+from pathlib import Path
 
-version = importlib.import_module('.version', PLUGIN_NAME)
+with open(str(Path(__file__).parent.joinpath(f'src/{PLUGIN_NAME}/VERSION')), 'r') as fvers:
+    version = fvers.read().strip()
+
 
 with open('README.rst') as fd:
     long_description = fd.read()
 
 setupOpts = dict(
     name=PLUGIN_NAME,
-    description=description,
+    description=config['plugin-info']['description'],
     long_description=long_description,
-    license='CECILL B',
-    url=package_url,
-    author=author,
-    author_email=author_email,
+    license=config['plugin-info']['license'],
+    url=config['plugin-info']['package-url'],
+    author=config['plugin-info']['author'],
+    author_email=config['plugin-info']['author-email'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 5 - Production/Stable",
@@ -33,7 +30,7 @@ setupOpts = dict(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
         "Topic :: Scientific/Engineering :: Visualization",
-        "License :: CeCILL-B Free Software License Agreement (CECILL-B)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: User Interfaces",
@@ -41,14 +38,12 @@ setupOpts = dict(
 
 
 setup(
-    version=version.get_version(),
-    packages=find_packages(),
-    package_data={'': ['*.dll']},
+    version=version,
+    packages=find_packages(where='./src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}'},
-    install_requires=[
-        'pymodaq>=2.0',
-        ]+packages_required,
+    install_requires=['toml', ]+config['plugin-install']['packages-required'],
     **setupOpts
 )
 
