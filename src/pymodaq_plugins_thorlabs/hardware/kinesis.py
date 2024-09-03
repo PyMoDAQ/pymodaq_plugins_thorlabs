d736760e801a39a723b5f9e3e77edc59d9cd0af1
nano713 <50872819+nano713@users.noreply.github.com>
2024-09-03 10:29:14 +0900

    Refactor move_abs. Delete the second argument in SetOutVoltage.

--- src/pymodaq_plugins_thorlabs/hardware/kinesis.py
+++ src/pymodaq_plugins_thorlabs/hardware/kinesis.py
@@ -158,17 +158,12 @@ class Piezo(Kinesis):
             raise ValueError('Invalid Serial Number')
     
     def move_abs(self, position : float, callback = None):
-        print(f"type of position: {type(position)}")
-        if callback is not None: 
-            callback = Action[UInt64](callback)
-        else: 
-            callback = UInt64(0) 
-            min_volt = 0.0 #TODO: Check is value converts from float to Decimal. 
-            max_volt = Decimal.ToDouble(self._device.GetMaxOutputVoltage()) # float
-            if position >= min_volt and position <= max_volt:
-                self._device.SetOutputVoltage(Decimal(position), callback) #TODO: check if needs one command or two allowed
-            else:
-                raise ValueError('Invalid Voltage')
+        min_volt = 0.0 #TODO: Check is value converts from float to Decimal. 
+        max_volt = Decimal.ToDouble(self._device.GetMaxOutputVoltage()) # float
+        if position >= min_volt and position <= max_volt:
+            self._device.SetOutputVoltage(Decimal(position)) #TODO: check if needs one command or two allowed
+        else:
+            raise ValueError('Invalid Voltage')
 
     def move_home(self): 
         self.move_abs(0.0) #Not a precise home value. 
