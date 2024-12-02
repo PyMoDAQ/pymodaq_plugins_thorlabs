167696c5b766541e56dceb69ca2601cbb8d303b4
asthak <asthak@iisc.ac.in>
2024-12-02 14:42:59 +0900

    ccsxxx hardware update

--- src/pymodaq_plugins_thorlabs/hardware/ccsxxx.py
+++ src/pymodaq_plugins_thorlabs/hardware/ccsxxx.py
@@ -4,21 +4,31 @@ import numpy as np
 
 class CCSXXX:
     def __init__(self, resource_name):
-        self.dll_path = dll_path # DK - Replace dll_path with C://Program Files//IVI Foundation//VISA//Win64//Bin...?
-        self.resource_name = resource_name.encode('utf-8')
+        self.dll_path = "C:\Program Files\IVI Foundation\VISA\Win64\Bin"  # DK - Replace dll_path with C://Program Files//IVI Foundation//VISA//Win64//Bin...?
+        self.resource_name = resource_name#.encode('utf-8')
         self.lib = None
         self.ccs_handle = ctypes.c_int(0)
+        # def load_library(self):
         os.chdir(self.dll_path)
         self.lib = ctypes.cdll.LoadLibrary("TLCCS_64.dll")
 
-    # def load_library(self):
+
 
     def connect(self):
+        # connect to the device using DLL's init function'
         self._device = self.lib.tlccs_init(self.resource_name, 1, 1, ctypes.byref(self.ccs_handle))
         if self._device != 0:
             raise Exception("Failed to initialize the device")
 
     def set_integration_time(self, integration_time):
+        """
+
+        Args:
+            integration_time: in seconds
+
+        Returns:
+
+        """
         integration_time = ctypes.c_double(integration_time)
         status = self.lib.tlccs_setIntegrationTime(self.ccs_handle, integration_time)
         if status != 0:
@@ -29,7 +39,9 @@ class CCSXXX:
         if status != 0:
             raise Exception(f"Error starting scan: {status}")
 
-    def get_wavelength_data(self):
+    def get_wavelength_data(self, wavelengths=None):
+        wavelengths = np.zeros(3648, dtype=np.float64)
+        # wavelengths_c = wavelengths.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
         wavelengths = (ctypes.c_double * 3648)()
         status = self.lib.tlccs_getWavelengthData(self.ccs_handle, 0, ctypes.byref(wavelengths), ctypes.c_void_p(None), ctypes.c_void_p(None))
         if status != 0:
@@ -46,11 +58,14 @@ class CCSXXX:
         return data_array
 
     def close(self):
-        pass # DK - replace pass with the command.
+        self.lib.tlccs_close(self.ccs_handle)
+        # DK - replace pass with the command.
+
+    #  self.lib.tlccs_close(self.ccs_handle)  # when writing your own plugin replace this line
 
 # Example usage
 if __name__ == "__main__":
-    spectrometer = TLCCS(r"C:\Program Files\IVI Foundation\VISA\Win64\Bin", 'USB0::0x1313::0x8087::M00934802::RAW')
+    spectrometer = CCSXXX(r"C:\Program Files\IVI Foundation\VISA\Win64\Bin", 'USB0::0x1313::0x8087::M00934802::RAW')
     spectrometer.load_library()
     spectrometer.connect()
     spectrometer.set_integration_time(10.0e-3)
