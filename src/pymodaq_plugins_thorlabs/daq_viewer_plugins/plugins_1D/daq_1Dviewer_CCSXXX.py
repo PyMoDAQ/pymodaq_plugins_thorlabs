8fa9efed867bae83e1597130202f263f9f0ded09
nano713 <50872819+nano713@users.noreply.github.com>
2024-12-02 17:03:01 +0900

    Test and debug

--- src/pymodaq_plugins_thorlabs/daq_viewer_plugins/plugins_1D/daq_1Dviewer_CCSXXX.py
+++ src/pymodaq_plugins_thorlabs/daq_viewer_plugins/plugins_1D/daq_1Dviewer_CCSXXX.py
@@ -1,65 +1,33 @@
 import numpy as np
-# from pymodaq.utils.daq_utils import ThreadCommand
+from pymodaq.utils.daq_utils import ThreadCommand
 from pymodaq.utils.data import DataFromPlugins, Axis, DataToExport
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
 from pymodaq.utils.parameter import Parameter
 from pymodaq_plugins_thorlabs.hardware.ccsxxx import CCSXXX
 
-
-# DK - we created this in hardware/ccsxxx.py
-# class PythonWrapperOfYourInstrument:
-#     #  TODO Replace this fake class with the import of the real python wrapper of your instrument
-#     pass
-
-# TODO:
-# (1) change the name of the following class to DAQ_1DViewer_TheNameOfYourChoice
-# (2) change the name of this file to daq_1Dviewer_TheNameOfYourChoice ("TheNameOfYourChoice" should be the SAME
-#     for the class name and the file name.)
-# (3) this file should then be put into the right folder, namely IN THE FOLDER OF THE PLUGIN YOU ARE DEVELOPING:
-#     pymodaq_plugins_my_plugin/daq_viewer_plugins/plugins_1D
 class DAQ_1DViewer_CCSXXX(DAQ_Viewer_base):
     """ Instrument plugin class for a 1D viewer.
-    
+
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
     DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
-
-    TODO Complete the docstring of your plugin with:
-        * The set of instruments that should be compatible with this instrument plugin.
-        * With which instrument it has actually been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
 
     Attributes:
     -----------
     controller: object
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
-         
-    # TODO add your particular attributes here if any
 
     """
-    # DK - add integration_time parameter (done)
-    params = comon_parameters+[
-        {'title': 'Integration time', 'name': 'integration_time', 'type': 'float', 'value': 10.0e-3},
-        {'title': 'Resource Name:', 'name': 'resource_name', 'type': 'str', 'value': b'USB0::0x1313::0x8087::M00934802::RAW'}      # {DK - add resource_name},
-
-        ## TODO for your custom plugin
-        # elements to be added here as dicts in order to control your custom stage
-        ############
-        ]
-    # integration time
+    params = comon_parameters + [
+        {'title': 'Integration time', 'name': 'integration_time', 'type': 'float', 'value': 100.0e-3}, # in seconds
+        {'title': 'Resource name', 'name': 'resource_name', 'type': 'str', 'value': 'USB0::0x1313::0x8087::M00934802::RAW'},
+    ]
 
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
+        """Initialize attributes for the DAQ_1DViewer_CCSXXX class."""
         self.controller: CCSXXX = None
-
-        # TODO declare here attributes you want/need to init with a default value
-
         self.x_axis = None
 
-    # DK - change settings
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
 
@@ -68,11 +36,8 @@ class DAQ_1DViewer_CCSXXX(DAQ_Viewer_base):
         param: Parameter
             A given parameter (within detector_settings) whose value has been changed by the user
         """
-        ## TODO for your custom plugin
         if param.name() == "integration_time":
-           self.controller.set_integration_time(self.settings['integration_time'])
-#        elif ...
-        ##
+            self.controller.set_integration_time(self.settings['integration_time'])
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -89,37 +54,28 @@ class DAQ_1DViewer_CCSXXX(DAQ_Viewer_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-
-        # raise NotImplemented  # TODO when writing your own plugin remove this line and modify the one below
         self.ini_detector_init(slave_controller=controller)
 
         if self.is_master:
+            self.controller = CCSXXX(self.settings['resource_name'])
+            self.controller.connect()
 
-            self.controller = CCSXXX(self.settings['resource_name'])  #instantiate you driver with whatever arguments are needed
-            self.controller.connect() # call eventual methods
+        data_x_axis = self.controller.get_wavelength_data()
+        self.x_axis = Axis(data=data_x_axis, label='Wavelength', units='nm', index=0)
 
-        ## TODO for your custom plugin
-        # get the x_axis (you may want to to this also in the commit settings if x_axis may have changed
-        data_x_axis = self.controller.get_wavelength_data()  # if possible
-        self.x_axis = Axis(data=data_x_axis, label='wavelength', units='nm', index=0)
-
-        # TODO for your custom plugin. Initialize viewers pannel with the future type of data
         self.dte_signal_temp.emit(DataToExport(name='CCSXXX',
-                                               data=[DataFromPlugins(name='spectrum',
-                                                                     data=np.zeros((3648)),  # for example
+                                               data=[DataFromPlugins(name='Spectrum',
+                                                                     data=np.zeros((3648)),
                                                                      dim='Data1D', labels=['Intensity'],
                                                                      axes=[self.x_axis])]))
 
-        info = "CCSXXX spectrometer"
+        info = "CCSXXX spectrometer initialized"
         initialized = True
         return info, initialized
 
     def close(self):
         """Terminate the communication protocol"""
-        ## TODO for your custom plugin
-         # when writing your own plugin remove this line
         self.controller.close()
-        #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
 
     def grab_data(self, Naverage=1, **kwargs):
         """Start a grab from the detector
@@ -132,37 +88,17 @@ class DAQ_1DViewer_CCSXXX(DAQ_Viewer_base):
         kwargs: dict
             others optionals arguments
         """
-        ## TODO for your custom plugin: you should choose EITHER the synchrone or the asynchrone version following
-
-        ##synchrone version (blocking function)
         self.controller.start_scan()
         data_tot = self.controller.get_scan_data()
         self.dte_signal.emit(DataToExport('CCSXXX',
                                           data=[DataFromPlugins(name='Spectrum', data=data_tot,
-                                                                dim='Data1D', labels=['data'],
+                                                                dim='Data1D', labels=['Intensity'],
                                                                 axes=[self.x_axis])]))
-
-        ##asynchrone version (non-blocking function with callback)
-        # self.controller.start_scan(self.callback)
-
-
-
-    # def callback(self):
-        """optional asynchrone method called when the detector has finished its acquisition of data"""
-        # data_tot = self.controller.get_scan_data()
-        # self.dte_signal.emit(DataToExport('myplugin',
-        #                               data=[DataFromPlugins(name='Mock1', data=data_tot,
-        #                                                        dim='Data1D', labels=['dat0', 'data1'])]))
 
     def stop(self):
         """Stop the current grab hardware wise if necessary"""
-        ## TODO for your custom plugin
         # raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.close()  # when writing your own plugin replace this line
-        # self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
-        # ##############################
         return ''
-
 
 if __name__ == '__main__':
     main(__file__)
--- src/pymodaq_plugins_thorlabs/hardware/ccsxxx.py
+++ src/pymodaq_plugins_thorlabs/hardware/ccsxxx.py
@@ -1,25 +1,19 @@
 import os
 import ctypes
 import numpy as np
+
 dll_path = r"C:\Program Files\IVI Foundation\VISA\Win64\Bin"
 os.chdir(dll_path)
-lib=ctypes.cdll.LoadLibrary("TLCCS_64.dll")
+lib = ctypes.cdll.LoadLibrary("TLCCS_64.dll")
 
 class CCSXXX:
-    def __init__(self, resource_name):
-        # self.dll_path = r"C:\Program Files\IVI Foundation\VISA\Win64\Bin"  # DK - Replace dll_path with C://Program Files//IVI Foundation//VISA//Win64//Bin...?
-        self.resource_name = resource_name#.encode('utf-8')
-        # self.lib = None
+    def __init__(self, rsrc_name):
+        self.rsrc_name = rsrc_name.encode('utf-8')
         self.ccs_handle = ctypes.c_int(0)
-        # def load_library(self):
-        # os.chdir(self.dll_path)
-        # self.lib = ctypes.cdll.LoadLibrary("TLCCS_64.dll")
-
-
 
     def connect(self):
         # connect to the device using DLL's init function'
-        self._device = lib.tlccs_init(self.resource_name, 1, 1, ctypes.byref(self.ccs_handle))
+        self._device = lib.tlccs_init(self.rsrc_name, 1, 1, ctypes.byref(self.ccs_handle))
         if self._device != 0:
             raise Exception("Failed to initialize the device")
 
@@ -43,27 +37,23 @@ class CCSXXX:
             raise Exception(f"Error starting scan: {status}")
 
     def get_wavelength_data(self):
-        wavelengths = np.zeros(3648, dtype=np.float64)
-        # wavelengths_c = wavelengths.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
         wavelengths = (ctypes.c_double * 3648)()
         status = lib.tlccs_getWavelengthData(self.ccs_handle, 0, ctypes.byref(wavelengths), ctypes.c_void_p(None), ctypes.c_void_p(None))
         if status != 0:
             raise Exception(f"Error getting wavelength data: {status}")
         wavelengths = np.array(list(wavelengths))
-        return wavelengths 
+        return wavelengths
 
     def get_scan_data(self):
-
         data_array = (ctypes.c_double * 3648)()
         status = lib.tlccs_getScanData(self.ccs_handle, ctypes.byref(data_array))
         if status != 0:
             raise Exception(f"Error getting scan data: {status}")
-        data_array= np.array(list(data_array))
+        data_array = np.array(list(data_array))
         return data_array
 
     def close(self):
         lib.tlccs_close(self.ccs_handle)
-        # DK - replace pass with the command.
 
     #  self.lib.tlccs_close(self.ccs_handle)  # when writing your own plugin replace this line
 
