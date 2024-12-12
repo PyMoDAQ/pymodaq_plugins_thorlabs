c7a0ac9e6714e14c55f3962b89db07f182b12392
nano713 <50872819+nano713@users.noreply.github.com>
2024-12-12 22:09:03 +0900

    Update params

--- src/pymodaq_plugins_thorlabs/daq_move_plugins/daq_move_KDC101.py
+++ src/pymodaq_plugins_thorlabs/daq_move_plugins/daq_move_KDC101.py
@@ -35,9 +35,6 @@ class DAQ_Move_KDC101(DAQ_Move_base):
     params = [
                  {'title': 'Serial Number:', 'name': 'serial_number', 'type': 'list',
                   'limits': serialnumbers_kdc101, 'value': serialnumbers_kdc101[0]},
-                {'title': 'Units:', 'name': 'units', 'type': 'list', "limits": ["mm", "um", "m", "nm"], 
-                    "value": 'mm'}
-
              ] + comon_parameters_fun(is_multiaxes, axes_names=_axis_names, epsilon=_epsilon)
 
     def ini_attributes(self):
