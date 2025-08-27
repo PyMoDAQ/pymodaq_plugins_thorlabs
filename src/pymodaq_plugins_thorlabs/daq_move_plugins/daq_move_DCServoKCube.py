e5708e2cc1b59fdf199fd0e928e77bf80ed5823e
Sébastien Weber <seba.weber@gmail.com>
2025-08-27 10:13:08 +0200

    Update daq_move_DCServoKCube.py

--- src/pymodaq_plugins_thorlabs/daq_move_plugins/daq_move_DCServoKCube.py
+++ src/pymodaq_plugins_thorlabs/daq_move_plugins/daq_move_DCServoKCube.py
@@ -1,12 +1,13 @@
 from pymodaq.control_modules.move_utility_classes import (
     DAQ_Move_base, comon_parameters_fun, main, DataActuatorType, DataActuator)
-from pymodaq.utils.daq_utils import ThreadCommand
 
-from pymodaq.utils.parameter import Parameter
+from pymodaq_utils.logger import set_logger, get_module_name
+from pymodaq_utils.utils import ThreadCommand
+from pymodaq_gui.parameter import Parameter
 
 from pymodaq_plugins_thorlabs.hardware.daq_move_servocube_abstract import DAQ_Move_DCServoCube_Abstract
 from pymodaq_plugins_thorlabs.hardware.kinesis import DCServoKCube, serialnumbers_kcube_dcservo
-from pymodaq.utils.logger import set_logger, get_module_name
+
 
 logger = set_logger(get_module_name(__file__))
 
